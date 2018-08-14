132d8bf1eb5a5432b35a3a53f8d9cc3ec2133a26
moniqu19 Campbell <monique.campbell1913@gmail.com>
2018-08-14 20:27:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,6 +6,15 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+
+
   # Add your post route and action below
+
+
+post '/food' do
+  
+" My name is #{params[:name]}, and I love #{params[:pizza]}"
+end
+
 
 end
